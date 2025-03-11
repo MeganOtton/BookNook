705cf3f0966ebe8197e7fed3a3e40c7475e1ad4c
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-03-11 17:44:47 +0000

    [HTML,CSS] Genre in Model ADD

--- Store/models.py
+++ Store/models.py
@@ -5,6 +5,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Capitalize the first letter of the genre name
+        self.name = self.name.capitalize()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name  # To display the readable name
+
 class Topic(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
@@ -14,30 +25,31 @@ class Topic(models.Model):
 # Create your models here.
 class BookStorePage(models.Model):
     
-    booktitle = models.CharField(max_length=200, unique=True)
+    booktitle = models.CharField(max_length=200, unique=True, verbose_name="Book Title")
     
-    slug = models.SlugField(max_length=200, unique=True, blank=True)
-    
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = self.booktitle
-        super().save(*args, **kwargs)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, verbose_name="SYSTEM, DO NOT EDIT")
 
-    authorname = models.CharField(max_length=200, null=True, blank=True, default='')
+    authorname = models.CharField(max_length=200, null=True, blank=True, default='', verbose_name="Author Name")
     
-    genre = models.CharField(max_length=200, null=True, blank=True, default='')
+    genre = models.ManyToManyField(Genre, related_name='books', blank=True, verbose_name="Genre")
 
-    chapter = models.IntegerField(default=0)
+    age_restriction = models.BooleanField(
+        choices=[(True, 'Yes'), (False, 'No')],
+        default=False,
+        verbose_name="Age Restriction (18+)"
+    )
+
+    chapter = models.IntegerField(default=0, verbose_name="Amount Of Chapters")
 
     bookcover = CloudinaryField('image', default='placeholder')
 
-    bookdescription = models.TextField()
+    bookdescription = models.TextField(verbose_name="Book Description")
 
-    bookprice = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    bookprice = models.DecimalField(max_digits=6, decimal_places=2, default=0.00, verbose_name="Book Price")
 
-    bookrating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+    bookrating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], verbose_name="Book Rating")
 
-    topics = models.ManyToManyField('Topic', related_name='books', blank=True)
+    topics = models.ManyToManyField('Topic', related_name='books', blank=True, verbose_name="Topics")
 
     # series = models.ManyToManyField('Series', related_name='books')
 
@@ -46,6 +58,14 @@ class BookStorePage(models.Model):
     # )
 
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.booktitle
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.booktitle
 
 
 # class Post(models.Model):
