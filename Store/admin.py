814eec19898b89c4a000a2219dc0fbc1c4e4a5f2
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-03-11 19:44:16 +0000

    [HTML, PY] Topics ADD

--- Store/admin.py
+++ Store/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from .models import BookStorePage, Genre
+from .models import BookStorePage, Genre, Topic
 
 
 # Register your models here.
 @admin.register(BookStorePage)
 class BookStorePageAdmin(admin.ModelAdmin):
     list_display = ('booktitle', 'authorname', 'bookprice', 'bookrating', 'status')
-    filter_horizontal = ('genre',)  # Better interface for selecting multiple genres
+    filter_horizontal = ('genre','topics')  # Better interface for selecting multiple genres
     search_fields = ('booktitle', 'authorname', 'bookprice', 'bookrating')
     prepopulated_fields = {'slug': ('booktitle',)}
     ordering = ['status' ]
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Topic)
+class TopicsAdmin(admin.ModelAdmin):
     list_display = ('name',)
