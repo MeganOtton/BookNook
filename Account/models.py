450edc24a0285a4a4382af9e648c018f7e677c10
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-03-14 18:22:38 +0000

    [PY] Account Type ADD

--- Account/models.py
+++ Account/models.py
@@ -8,9 +8,37 @@ from django.utils import timezone
 class Account (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userprofile')
     purchased_books = models.ManyToManyField(BookStorePage, related_name='purchased_books', blank=True)
+    birthdate = models.DateField(null=True, blank=True, verbose_name="Birthdate")
+
+    
+    # Role field (default is 'child', optional role is 'author')
+    ROLE_CHOICES = [
+        ('child', 'Child Reader'),
+        ('adult', 'Adult Reader'),
+        ('author', 'Author'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='adult')
 
     def __str__(self):
         return self.user.username
+
+    # Automatically set the role based on the birthdate
+    def save(self, *args, **kwargs):
+        if self.birthdate:
+            # Calculate age based on the birthdate
+            today = timezone.now().date()
+            age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+            
+            # Set role based on age
+            if age < 18:
+                self.role = 'child'
+            else:
+                 # Check if the role is 'author' and ensure the user is over 18
+                if self.role == 'author' and age < 18:
+                    raise ValueError("User must be over 18 to be an author.")
+                self.role = 'adult'
+        
+        super().save(*args, **kwargs)
 
 # Create or update the user profile
 # This function creates or updates the user profile
