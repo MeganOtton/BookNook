3adfaee09e322b98b7603a6c37a885f4545d717b
Megan Otton <115323221+MeganOtton@users.noreply.github.com>
2025-03-22 17:56:46 +0000

    Removed Device_Detection.html

--- Store/urls.py
+++ Store/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.urls import path
-from .views import device_detection_view, BookList
+from .views import BookList
 
 urlpatterns = [
-    # path('', device_detection_view, name='device_detection'),
     path('', BookList.as_view(), name='index'),  # Assuming BookList is your index view
     path('search/', views.BookListSearch.as_view(), name='book_search'),
     path('<slug:slug>/', views.book_details, name='book_details_list'),
